c12240f9515ed9fda808396266c7369b6b61b132
Samir <samlegdah@gmail.com>
2017-05-11 21:21:14 +0200

    insert data of sale

--- src/main/connect/sale.php
+++ src/main/connect/sale.php
@@ -1,13 +1,15 @@
 <?php
 
     if($_POST['id_user'] && $_POST['title']
-            && $_POST['price']&& $_POST['date'])
+            && $_POST['price']&& $_POST['date'] && $_POST['faculty'])
     {
         $idUser = $_POST['id_user'];
         $title = $_POST['title'];
         $price = $_POST['price'];
         $date = $_POST['date'];
+        $faculty = $_POST['faculty'];
         //parametri facoltativi
+        $author = $_POST['author'];
         $isb = $_POST['isb'];
         $description = $_POST['description'];
         $image = $_POST['image'];
@@ -15,7 +17,7 @@
         include "../connect/db_connect.php";
 
         $sql="INSERT INTO unibg_books
-            . VALUES ('$idUser', '$title', '$isbn', '$price', '$description', '$image', '$date')";
+              VALUES ('$idUser', '$title', '$isbn', '$price', '$description', '$image', '$date')";
 
         if(mysqli_query($conn, $sql))
             echo true;
--- web/app/vendi.js
+++ web/app/vendi.js
@@ -5,15 +5,39 @@ Bl.vendi = {};
 
 Bl.vendi.render = (function()
 {
-   console.log("sono in vendi");
    $("#body-page").empty();
+   
+   Bl.idUser = "Samir";
    
    Bl.vendi.appendHtml();
    
+   $("#btn-sale-back").click(function()
+   {
+      Bl.main.render(); 
+   });
+   
    $('#btn-sale-next').click(function()
    {
+       var param = {};
+       param['id_user'] = Bl.idUser;
+       param['title'] = $("#sale-title").val();
+       param['author'] = $("#sale-author").val();
+       param['price'] = $("#sale-price").val();
+       param['description'] = $("#sale-description").val();
+       param['image'] = null;
+       param['date'] = new Date();
+       param['faculty'] = $("#sale-faculty").val();
        
-       Bl.vendi.publish
+       var required = ['title', 'price'];
+       
+       if(Bl.vendi.controlData(param, required))
+       {
+            Bl.vendi.publish(param);
+       }
+       else
+       {
+           alert("Completare i campi obbligatori");
+       }
    });
    
    $("#body-page").trigger("create");
@@ -36,12 +60,12 @@ Bl.vendi.appendHtml = (function()
     $("#body-page").append('</div>');
     
     $("#body-page").append('<div class="form-group sale-form"><label id="text-description"> Desrizione</label>');
-    $("#body-page").append('<textarea id="sale-description" class="form-control" rows="3" \n\
+    $("#body-page").append('<textarea id="sale-description" class="form-control" rows="2" \n\
                     placeholder= "descrizione facoltativa ..."></textarea> ');
     $("#body-page").append('</div>');
     
     var html = '<label id="text-faculty" >Facolta</label>\
-                <select id="signin-faculty">\n\
+                <select id="sale-faculty">\n\
                     <option>Ingegneria</option>\n\
                     <option>Economia</option>\n\
                     <option>Letteratura</option>\n\
@@ -53,6 +77,18 @@ Bl.vendi.appendHtml = (function()
     
 });
 
+Bl.vendi.controlData = function(param, required)
+{
+    for(var i =0; i < required.length; i++)
+    {
+        if($.isEmptyObject(param[required[i]]))
+        {
+            return false;
+        }
+    }
+    return true;
+};
+
 Bl.vendi.publish= (function(param)
 {
     var pathParts = window.location.href.split("/");
@@ -60,15 +96,17 @@ Bl.vendi.publish= (function(param)
     
     var dati = "id_user="+param['id_user']+
             "&title="+param['title']+
+            "&author="+param['author']+
             "&isbn="+param['isbn']+
             "&price="+param['price']+
             "&description="+param['description']+
             "&image="+param['image']+
-            "&date="+param['date'];
+            "&date="+param['date']+
+            "&faculty="+param['faculty'];
     
     $.ajax({
         type: "POST",
-        url: root+"/src/main/connect/signin.php",
+        url: root+"/src/main/connect/sale.php",
         data: dati,
         dataType: "html",
 
@@ -77,8 +115,7 @@ Bl.vendi.publish= (function(param)
             if(msg==="1")
             {
                 alert("Annuncio pubblicato correttamente");
-                Bl.configuration.setCookie("bluni-cookie", dati["user"]);
-                Bl.configuration.start();
+                Bl.main.render();
             }	
             else
             {
