0d9497475d4d9747afa0f3c512040afcafbdd4ac
Samir <samlegdah@gmail.com>
2017-05-14 15:26:30 +0200

    comunicazione POST con json in sale

--- src/main/connect/sale.php
+++ src/main/connect/sale.php
@@ -1,32 +1,23 @@
 <?php
 
-    if($_POST['id_user'] && $_POST['title']
-            && $_POST['price']&& $_POST['date'] && $_POST['faculty'])
-    {
-        $idUser = $_POST['id_user'];
-        $title = $_POST['title'];
-        $price = $_POST['price'];
-        $date = $_POST['date'];
-        $faculty = $_POST['faculty'];
-        //parametri facoltativi
-        $author = $_POST['author'];
-        $isb = $_POST['isb'];
-        $description = $_POST['description'];
-        $image = $_POST['image'];
+//header("Content-Type: application/json; charset=UTF-8");
+if(isset($_POST['param']))
+{
+    $obj = json_decode($_POST['param'], false);
         
-        include "../connect/db_connect.php";
+    include "../connect/db_connect.php";
 
-        $sql="INSERT INTO unibg_books
-              VALUES ('$idUser', '$title', '$isbn', '$price', '$description', '$image', '$date')";
+    $sql="INSERT INTO unibg_books
+          VALUES ('$obj->id_user', '$obj->title', '$obj->author', "
+            . "'$obj->price', '$obj->description', '$obj->image', '$obj->faculty', '$obj->date')";
 
-        if(mysqli_query($conn, $sql))
-            echo true;
-        else
-            echo "[ERRORE] errore nell'inserimento dei dati ".mysqli_error($conn);
-    }
+    if(mysqli_query($conn, $sql))
+        echo true;
     else
-    {
-        echo "[ERRORE] dati non inseriti correttamente";
-    }
-
-	
+        echo "[ERRORE] query non avviata: ".mysqli_error($conn);
+    
+}
+else
+{
+    echo "[ERRORE] parametri non passati correttamente ".$_POST['param'];
+}
